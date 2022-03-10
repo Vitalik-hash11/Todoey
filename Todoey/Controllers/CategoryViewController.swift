40e6ab6aff81fed9191b3165d808d248164dcfda
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2022-03-10 23:00:44 +0200

    add category table view

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,81 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by newbie on 10.03.2022.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var categoryList = [Category]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadCategories()
+    }
+    
+    //MARK: - Adding new categories
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { alertAction in
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+            self.categoryList.append(newCategory)
+            
+            self.saveCategories()
+        }
+        
+        alert.addAction(action)
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Category name"
+            textField = alertTextField
+        }
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryList.count
+    }
+    
+    //MARK: - Table view delegate methods
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+        cell.textLabel?.text = categoryList[indexPath.row].name
+        return cell
+    }
+    
+    
+    //MARK: - Data model manipulation methods
+    
+    func saveCategories() {
+        do {
+            try context.save()
+        } catch {
+            print("Storing context failed")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadCategories() {
+        let request: NSFetchRequest<Category> = NSFetchRequest(entityName: "Category")
+        
+        do {
+            categoryList = try context.fetch(request)
+        } catch {
+            print("Fetching categories failed")
+        }
+    }
+}
