de26e44481271f0c7ebe9fd11193d2bdb2ee056c
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2022-03-10 21:42:38 +0200

    add core data functionality

---
+++ Todoey/Controllers/ToDoViewController.swift
@@ -0,0 +1,123 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ToDoViewController: UITableViewController {
+
+    var itemList = [Item]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        loadData()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemList.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoIdentifier", for: indexPath)
+        cell.textLabel?.text = itemList[indexPath.row].title
+        cell.accessoryType = itemList[indexPath.row].done ? .checkmark : .none
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //deleting
+//        let removed = itemList.remove(at: indexPath.row)
+//        context.delete(removed)
+        itemList[indexPath.row].done = !itemList[indexPath.row].done
+        
+        saveItems()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    //MARK: - Add new action
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new todo item?", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add todo", style: .default) { (alertAction) in
+            if  textField.text! != "" {
+                let newItem = Item(context: self.context)
+                newItem.title = textField.text!
+                newItem.done = false
+                self.itemList.append(newItem)
+                self.saveItems()
+            }
+        }
+        
+        alert.addTextField { alerttextField in
+            alerttextField.placeholder = "Add new todo"
+            textField = alerttextField
+        }
+        alert.addAction(action);
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    //MARK: - Saving items in user defaults
+    
+    func saveItems() {
+        do {
+            try context.save()
+        } catch {
+            print("Savint itemList failed")
+        }
+    
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadData(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+        do {
+            itemList = try context.fetch(request)
+        } catch {
+            print("Fetching items from db failed")
+        }
+        tableView.reloadData()
+    }
+}
+
+extension ToDoViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadData(with: request)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        print("Cancel")
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText == "" {
+            loadData()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        } else {
+            searchBarSearchButtonClicked(searchBar)
+        }
+    }
+}
+
